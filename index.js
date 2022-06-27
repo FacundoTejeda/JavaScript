248e523cb64478ddae8623d83338f91dffda1aea
facu tejeda <facundotejeda3@gmail.com>
2022-06-26 21:36:38 -0300

    intentando que funcionen mi switch

--- index.js
+++ index.js
@@ -48,14 +48,27 @@
 let  total = calculo (cantidad, sesionEgresados)
 alert ("El total a abonar es de: $" + total)
  
+let porcentajeDescuento1 = parseFloat (0.15)
+let porcentajeDescuento2 = parseFloat (0.05)
+
+function precioFinal1 (total, porcentajeDescuento1){
+    return (total - (total * porcentajeDescuento1))          
+    }
+    let precioFinal_1 =  precioFinal1 ()
+    
+    function precioFinal2 (total, porcentajeDescuento2){
+        return (total - (total * porcentajeDescuento2)) 
+        }
+    let precioFinal_2 =  precioFinal2()
+
 
 let formaPago = parseInt(prompt("En que forma van a querer abonar?: \n1. Si abonan en una cuota tienen un (15% de descuento) \n2. Si abonan en 3 cuotas tienen un (5% de descuento) \n3. Si abonan en 6 cuotas no tienen descuento" ))
 switch(formaPago) {
     case 1:      
-        alert ("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal_1);
+        alert ("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal1);
         break;
     case 2:        
-        alert ("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal_2);
+        alert ("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal2);
         break;
     case 3:        
         alert ("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + total);
@@ -65,16 +78,4 @@ switch(formaPago) {
         break;
 }
 
-let porcentajeDescuento1 = 0.15;
-let porcentajeDescuento2 = 0.05;
-
-function precioFinal1 (total, porcentajeDescuento1){
-    return (total - (total * porcentajeDescuento1))          
-    }
-    let precioFinal_1 = precioFinal1 ()
-    
-    function precioFinal2 (total, porcentajeDescuento2){
-        return (total - (total * porcentajeDescuento2)) 
-        }
-    let precioFinal_2 = precioFinal2()
 

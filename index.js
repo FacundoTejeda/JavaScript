1d9bfa2d0a33db680d0eec38618eb53f9606cbeb
facu tejeda <facundotejeda3@gmail.com>
2022-07-03 16:32:37 -0300

    modificando proyecto

--- index.js
+++ index.js
@@ -15,59 +15,109 @@
 //    while(i<=10)
  
 
-//    let nombres = prompt("Ingresa el nombre de tu colegio: \n1.Instituto Adventista Cordoba,  \n2.Instituto Adventista Balcarce,  \n3.Instituto Adventista Santa Fe")
-//   while(nombres !="" ){
-//        switch (nombres) {
-//            case "1":
-//                alert ("Hola chicos de Cordoba");
-//                break;
-//            case "2":
-//                alert ("Hola chicos de Balcarce");
-//                break;
-//            case "3":
-//                alert ("Hola chicos de Santa Fe");
-//                break;
-//            default:
-//                alert ("De que cole sos?");
-//                break;
-//        }
-//        nombres = prompt ("Ingresa tu colegio");
-//    }
-//   const sesionEgresados = 10000
-//   let cantidad = parseInt(prompt("Ingresar cantidad de alumnos que quieren la sesion (Hay descuentos por cantidad): "))
-//   function  calculo(cantidad, sesionEgresados){
-//      return cantidad * sesionEgresados;
-//   }
-//  let  total = calculo(cantidad, sesionEgresados)
-//  alert("El total a abonar es de: $" + total)
- 
-//  let porcentajeDescuento1 = parseFloat(0.15)
-//  let porcentajeDescuento2 = parseFloat(0.05)
-// function precioFinal1(total, porcentajeDescuento1){
-//      return(total - (total * porcentajeDescuento1))          
-//      }
-//      let precioFinal_1 =  precioFinal1(total, porcentajeDescuento1)
-    
-//      function precioFinal2(total, porcentajeDescuento2){
-//          return(total - (total * porcentajeDescuento2)) 
-//          }
-//      let precioFinal_2 =  precioFinal2(total, porcentajeDescuento2)
 
-//  let formaPago = parseInt(prompt("En que forma van a querer abonar?: \n1. Si abonan en una cuota tienen un (15% de descuento) \n2. Si abonan en 3 cuotas tienen un (5% de descuento) \n3. Si abonan en 6 cuotas no tienen descuento" ))
-//  switch(formaPago) {
-//      case 1:      
-//          alert("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal_1);
-//          break;
-//      case 2:        
-//          alert("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal_2);
-//          break;
-//      case 3:        
-//          alert("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + total);
-//          break;
-//      default:
-//          alert("Si estas buscando otra financiacion escribinos por privado");
-//          break;
-//  }
+let ingreso = prompt("Estas buscando cobertura para: \n1.Familias  \n2.Colegio")
+   while(ingreso !="" ){
+        switch (ingreso) {
+          case "1":
+                 let lastName1 = prompt("Ingresá el apellido de tu familia")
+                 let cantidadFamilia1 = parseFloat(prompt("Cuantos integrantes van a asistir?"))
+                 alert ("Bienvenida familia " +lastName1 + "Ustedes son " + cantidadFamilia1)           
+                 
+                 let lastName2 = prompt("Ingresá el apellido de tu familia")
+                 let cantidadFamilia2 = parseFloat(prompt("Cuantos integrantes van a asistir?"))
+                 alert ("Bienvenida familia " +lastName2 + "Ustedes son " + cantidadFamilia2)   
+
+                 let lastName3 = prompt("Ingresá el apellido de tu familia")
+                 let cantidadFamilia3 = parseFloat(prompt("Cuantos integrantes van a asistir?"))
+                 alert ("Bienvenida familia " +lastName3 + "Ustedes son " + cantidadFamilia3)     
+                 let lastName4 = prompt("Ingresá el apellido de tu familia")
+
+                 let cantidadFamilia4 = parseFloat(prompt("Cuantos integrantes van a asistir?"))
+                 alert ("Bienvenida familia " +lastName4 + "Ustedes son " + cantidadFamilia4)     
+                break;
+           case "2":
+                let nombres = prompt("Ingresa el nombre de tu colegio: \n1.Instituto Adventista Cordoba,  \n2.Instituto Adventista Balcarce,  \n3.Instituto Adventista Santa Fe")
+            while(nombres !="" ){
+         switch (nombres) {
+            case "1":
+                alert ("Hola chicos de Cordoba");
+                break;
+            case "2":
+                alert ("Hola chicos de Balcarce");
+                break;
+            case "3":
+                alert ("Hola chicos de Santa Fe");
+                break;
+            default:
+                alert ("De que cole sos?");
+               break;       
+       }
+        nombres = prompt ("Ingresa tu colegio");
+        break;        
+    }
+    }
+    }
+
+    const sesionEgresados = 10000
+    let cantidad = parseInt(prompt("Ingresar cantidad de alumnos que quieren la sesion (Hay descuentos por cantidad): "))
+    function  calculo(cantidad, sesionEgresados){
+       return cantidad * sesionEgresados;
+    }
+   let total = calculo(cantidad, sesionEgresados)
+   alert("El total a abonar es de: $" + total)
+  
+  let porcentajeDescuento1 = parseFloat(0.15)
+  let porcentajeDescuento2 = parseFloat(0.05)
+ function precioFinal1(total, porcentajeDescuento1){
+      return(total - (total * porcentajeDescuento1))          
+      }
+      let precioFinal_1 =  precioFinal1(total, porcentajeDescuento1)
+     
+      function precioFinal2(total, porcentajeDescuento2){
+          return(total - (total * porcentajeDescuento2)) 
+          }
+      let precioFinal_2 =  precioFinal2(total, porcentajeDescuento2)
+ 
+  let formaPago = parseInt(prompt("En que forma van a querer abonar?: \n1. Si abonan en una cuota tienen un (15% de descuento) \n2. Si abonan en 3 cuotas tienen un (5% de descuento) \n3. Si abonan en 6 cuotas no tienen descuento" ))
+  switch(formaPago) {
+      case 1:      
+          alert("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal_1);
+          break;
+      case 2:        
+          alert("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + precioFinal_2);
+          break;
+      case 3:        
+          alert("Muchas gracias por su pago, nos vemos en la fiesta!, el total es: $ " + total);
+          break;
+      default:
+          alert("Si estas buscando otra financiacion escribinos por privado");
+          break;
+  }
+
+
+
+
+  let precioCantidad = 2500
+
+    const egresados = [
+        {apellido: lastName1, cantidad: cantidadFamilia1, precio:cantidadFamilia1 *precioCantidad },
+        {apellido: lastName2, cantidad: cantidadFamilia2, precio:cantidadFamilia2 *precioCantidad },
+        {apellido: lastName3, cantidad: cantidadFamilia3, precio:cantidadFamilia3 *precioCantidad },
+        {apellido: lastName4, cantidad: cantidadFamilia4, precio:cantidadFamilia4 *precioCantidad },
+        
+    ]; 
+    
+    
+//     const cantidadTotal = egresados.reduce(
+//         (acumulador, elemento) => acumulador + elemento.cantidad,
+//         0
+//     );
+    
+//     alert("La cantidad de personas a fotografiar es: "+ cantidadTotal)
+
+
+
 
 
 // class Producto{
@@ -78,6 +128,7 @@
 //         this.cantidad = cantidad
 //     }
 // }
+// Esta funcion es para que los fotografos ingresen sus sesiones hechas
 
 // function agregarProductos(){
 //     let numeroSesiones = parseInt(prompt("Cuantas sesiones necesita ingresar"))
@@ -85,15 +136,16 @@
 
 //     for(let index = 0; index < numeroSesiones; index++){
 //         let nombre = prompt ("Ingrese su nombre")
-//         let precioCompra = parseFloat(prompt("Ingrese el precio de compra de los materiales"))
-//         let precioVenta = parseFloat(prompt("Ingrese el precio venta final"))
-//         let cantidad = parseInt(prompt("Ingrese la cantidad de egresados"))
+//         let precioCompra0 = parseFloat(prompt("Ingrese el precio de compra de los materiales"))
+//         let precioVenta0 = parseFloat(prompt("Ingrese el precio venta final"))
+//         let cantidad0 = parseInt(prompt("Ingrese la cantidad de egresados"))
 
-//         let sesionARegistrar = new Producto(nombre, precioCompra, precioVenta, cantidad)
+//         let sesionARegistrar = new Producto(nombre, precioCompra0, precioVenta0, cantidad0)
 //         sesiones.push(sesionARegistrar)
 //     }
 
 //     let productoNuevo = new Producto ("Producto 2", 100, 2000, 22)
+    
 //      sesiones.unshift(productoNuevo)
 
 // return sesiones
@@ -122,20 +174,7 @@
 // })
 
 
-const egresados = [
-    {apellido:"tejeda", cantidad: 4, precio:4000 },
-    {apellido:"perez", cantidad: 2, precio:2000 },
-    {apellido:"martinez", cantidad: 3, precio:3000 },
-    {apellido:"cerri", cantidad: 5, precio:5000 },
-]; 
 
-
-const total = egresados.reduce(
-    (acumulador, elemento) => acumulador + elemento.cantidad,
-    0
-);
-
-alert("La cantidad de personas a fotografiar es: "+ total)
 
 
 
@@ -162,7 +201,7 @@ alert("La cantidad de personas a fotografiar es: "+ total)
 // const listaFamilias = egresados.map((familia) => familia.apellido);
 // console.log(listaFamilias)
 
-// //
+// //   
 
 // const numeros = [1,2,3,40,24,33];
 

67f624930feaaf902e033a792265307eb7cda794
facu tejeda <facundotejeda3@gmail.com>
2022-07-13 00:18:11 -0300

    sacando cosas innecesarias de la pagina

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ let bienvenidaFamiliares  = document.createElement("div")
 
     const familiaresEgresados = [
         {apellido: inputApellido, cantidad: cantidad, precio:cantidad *precioCantidad },
-        // {apellido: apellido1, cantidad: cantidadFamilia2, precio:cantidadFamilia2 *precioCantidad },
-         
+                
     ]; 
     
     
@@ -35,121 +34,20 @@ let bienvenidaFamiliares  = document.createElement("div")
     console.log("La cantidad de personas a fotografiar es: "+ cantidadTotal)
 
 
- 
-let formularioAlumnos = document.getElementById("formularioAlumnos")
-let inputCurso = document.getElementById("inputCurso")
-let inputCantidadAlumnos = document.getElementById("inputCantidadAlumnos")
-
-
-let productosAlumnos = []
-
-class productosColegio{
-    constructor(curso, cantidad, precioPorAlumno ){
-    this.curso = this.curso;
-    this.cantidad = cantidad;
-  }
-}
-
-formularioAlumnos.onsubmit = (event) => validarFormularioAlumno(event);
-
-function validarFormularioAlumno(event){
-    event.preventDefault();
-    let curso = inputCurso.value;
-    let cantidad = inputCantidadAlumnos.value;
-    const precioPorAlumno = 10000; 
-    
-    let totalCurso = cantidad * precioPorAlumno
-
-    let productos1 = new productosColegio(curso, cantidad, precioPorAlumno)
-    productosAlumnos.push(productos1)
-
-    formularioAlumnos.reset();
-    console.log(totalCurso)
-}
-
-
-// let formaDePago = document.getElementById("formaPago")
-// let UnaCuota = document.getElementById("UnaC")
-// let TresCuota = document.getElementById("TresC")
-// let SeisCuota = document.getElementById("SeisC")
-
-// console.log(unaC.innerText)
-// unaC.innerText ="El pago en una cuota es de: " + precioFinal_1
-
-// console.log(TresC.innerText)
-// TresC.innerText ="El pago en una cuota es de: " + precioFinal_2
-
-// console.log(SeisC.innerText)
-// SeisC.innerText ="El pago en una cuota es de: " + total
-
-//     function  calculo(cantidad, sesionEgresados){
-//        return cantidad * sesionEgresados;
-//     }
-//    let total = calculo(cantidad, sesionEgresados)
- 
-  
-//   let porcentajeDescuento1 = parseFloat(0.15)
-//   let porcentajeDescuento2 = parseFloat(0.05)
-
-//  function precioFinal1(total, porcentajeDescuento1){
-//       return(total - (total * porcentajeDescuento1))          
-//       }
-//       let precioFinal_1 =  precioFinal1(total, porcentajeDescuento1)
-     
-//       function precioFinal2(total, porcentajeDescuento2){
-//           return(total - (total * porcentajeDescuento2)) 
-//           }
-//       let precioFinal_2 =  precioFinal2(total, porcentajeDescuento2)
- 
-//       let totalPrecio = document.getElementById("totalPrecio")
-//       console.log(totalPrecio.innerHTML)
-     
-      
-
-         
-//       let elPrecioFinal  = document.createElement("div")
-//       totalPrecio.innerHTML =`
-//          <div class="precioFinal">
-//             <h2> Pago con el 15% de descuento</h2>
-//             <p>$${precioFinal_1}</p>
-//          </div>               
-//          `
-//          totalPrecio.append(elPrecioFinal)
-//       break;
-//       case 2: 
-//          let elPrecioFinal2  = document.createElement("div")
-//          totalPrecio.innerHTML =`
-//          <div class="precioFinal">
-//             <h2> Pago con el 15% de descuento</h2>
-//             <p>$${precioFinal2}</p>
-//          </div>               
-//          `
-//          totalPrecio.append(elPrecioFinal2)       
-     
-//      break;
-//       case 3:        
-//       let elPrecioFinal3  = document.createElement("div")
-//       totalPrecio.innerHTML =`
-//          <div class="precioFinal">
-//             <h2> Pago con el 15% de descuento</h2>
-//             <p>$${total}</p>
-//          </div>               
-//          `
-//          totalPrecio.append(elPrecioFinal3)
-//      break;
-//       default:
-//           alert("Si estas buscando otra financiacion escribinos por privado");
-//      break;
-//   }
-
-
-
 // //Esta funcion es para que los fotografos ingresen sus sesiones hechas
+const restar = (a, b) => a - b;
+const div = (a, b) => a / b;
+const sumar = (a, b) => a + b;
+const multi = (a, b) => a * b;
+
+const gastosTotales = []
+
 let formularioFotografos = document.getElementById("formularioFotografos")
 let inputApellidoFotografo = document.getElementById("inputApellidoFotografo")
 let inputCantidad = document.getElementById("inputCantidad")
 let inputPrecioGastosFotografo = document.getElementById("inputPrecioGastosFotografo")
 let inputPrecioVentaFotografo = document.getElementById("inputPrecioVentaFotografo")
+let contenedorPresupuesto = document.getElementById("contenedor-presupuesto")
 
 let productosFotografo = []
 
@@ -160,6 +58,7 @@ class Productos{
     this.precioGastos = precioGastos;
     this.precioVenta = precioVenta;
   }
+  gananciaFotografo = () => ((this.cantidad * this.precioVenta) - this.precioGastos)
 }
 
 formularioFotografos.onsubmit = (event) => validarFormularioFotografo(event);
@@ -175,76 +74,23 @@ function validarFormularioFotografo(event){
     productosFotografo.push(producto)
 
     formularioFotografos.reset();
+    calcularGastos();
  
 }
 
-// function mostrarSesiones(formularioFotografos){
-//     for(const cantidad of cantidad){
-//         console.log(cantidad)
-        
-//     }
-// }
-
-// function main(){
-//     mostrarSesiones(productosFotografo)
-// }
-// main()
-
-// //inner text
-let parrafo = document.getElementById("parrafo1")
-console.log(parrafo.innerText)
-parrafo.innerText="Est es la descripcion modificada"
-console.log(parrafo.innerText)
-
-
-
-//
-let inputEmail = document.getElementById("inputEmail")
-let inputContrasenia = document.getElementById("inputContrasenia")
-
-console.log(inputEmail.value)
-inputEmail.value = "facudotejeda3@gmail.com"
-console.log(inputEmail.value)
-
-const listaProductos = [
-{
-    id:1,
-    nombre: "Graduacion",
-    precioGastos: "1000",
-    precioSesion: "5000",
-},
-{
-    id:2,
-    nombre: "Fiesta",
-    precioGastos: "900",
-    precioSesion: "6000",
-},
-{
-    id:3,
-    nombre: "Familiar",
-    precioGastos: "400",
-    precioSesion: "2500",
-},
-];   
-
-let contenedorProductos = document.getElementById("contenedor-productos")
-
-for(const producto of listaProductos){
-    let columna = document.createElement("div")
-    columna.className = "col-md-4 mt-3"
-    columna.id = `columna-${producto.id}`
-    columna.innerHTML =`
-        <div class="card">
-            <div class="card-body">
-                <p class="card-text">Nombre: <b>${producto.nombre}</b></p>           
-                <p class="card-text">Precio gastos: <b>${producto.precioGastos}</b></p>           
-                <p class="card-text">Precio Sesion: <b>${producto.precioSesion}</b></p>           
-            </div>    
-        </div>    `
-        
-contenedorProductos.append(columna)
+function calcularGastos () {
+    productosFotografo.forEach ((producto) =>{
+        let columna = document.createElement("div")
+        columna.innerHTML =`
+            <div class="card">
+                <div class="card_body">
+                    <p class="card_text">El total a liquidar es: $${Productos.gananciaFotografo}</p> 
+                </div>
+            </div>
+            `
+        calcularGastos.append (columna)
+    })
 }
-
 let botonUno = document.getElementById("btnEventoUno")
 let botonDos = document.getElementById("btnEventoDos")
 

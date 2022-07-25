3d089f072a24c56ced1c284395cefa26908ba1e3
facu tejeda <facundotejeda3@gmail.com>
2022-07-25 19:13:50 -0300

    sacando https

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ items.addEventListener("click", e =>{
 //FETCH DE MIS PRODUCTOS //
 const fetchData = async () => {
     try{
-        const res = await fetch ("http://jsonTest.json")
+        const res = await fetch ("jsonTest.json")
         const data = await res.json()  
         // console.log(data)      
         pintarCards(data)
